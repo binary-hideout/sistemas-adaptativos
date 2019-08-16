1cd1af3d506b0197bcc00338d05e34bfe2175567
netotz <neto.otz@hotmail.com>
2019-08-15 22:23:23 -0500

    Agregar recorrida de lista de colores RGB

--- tarea1/fuegos.py
+++ tarea1/fuegos.py
@@ -3,9 +3,12 @@ import random
 import pygame
 from sys import argv
 
+import numpy
+numpy.any
+
 pygame.init()
 
-def explotar(pos_x, pos_y, radio):
+def explotar(pos_x, pos_y, radio, rgb_tuple):
     print("Posición X:", pos_x)
     print("Posición Y:", pos_y)
     print("Radio:", radio, "\n")
@@ -19,20 +22,34 @@ def explotar(pos_x, pos_y, radio):
             distance = i * speed
             x = pos_x + distance * math.cos(angle)
             y = pos_y + distance * math.sin(angle)
-            screen.set_at((int(x), int(y)), (0,0,0))
+            screen.set_at((int(x), int(y)), rgb_tuple)
         pygame.display.flip()
 
-def secuencia_explosiones(ciclos):
-    if ciclos > 0:
+def secuencia_explosiones(ciclos, rgb_list):
+    for i in range(ciclos):
         pos_x = random.randint(100, 700)
         pos_y = random.randint(100, 700)
         radio = random.randint(100, 700)
 
-        explotar(pos_x, pos_y, radio)
-        secuencia_explosiones(ciclos - 1)
+        explotar(pos_x, pos_y, radio, rgb_list[i])
 
+def generarListaRGB(size):
+    '''Genera una lista de tuples. Cada tuple contiene un color aleatorio en formato RGB.
+    '''
+    rgb_list = list()
+    for i in range(size):
+        red = random.randint(0, 255)
+        green = random.randint(0, 255)
+        blue = random.randint(0, 255)
+        rgb = (red, green, blue)
+        rgb_list.append(rgb)
+    return rgb_list
+
+# validar si el comando contiene el argumento para la cantidad de explosiones
 try:
     arg_ciclos = int(argv[1])
 except:
     arg_ciclos = 3
-secuencia_explosiones(arg_ciclos)
+
+rgb_list = generarListaRGB(arg_ciclos)
+secuencia_explosiones(arg_ciclos, rgb_list)
