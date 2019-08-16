b0cd76f88098376f3e3dfc24a6db9367125b96a4
netotz <neto.otz@hotmail.com>
2019-08-15 21:20:03 -0500

    Agregar función 'explotar'

--- tarea1/fuegos.py
+++ tarea1/fuegos.py
@@ -1,17 +1,23 @@
 import math
 import random
 import pygame
-
 pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
-particles = [(random.gauss(0,.5), random.uniform(0,6.28318)) for i in range(2000)]
+def explotar(pos_x, pos_y, radio):
+    print("Posición X:", pos_x)
+    print("Posición Y:", pos_y)
+    print("Radio:", radio, "\n")
 
-for i in range(399):
-    screen.fill((255,255,255))
-    for speed, angle in particles:
-        distance = i * speed
-        x = 400 + distance * math.cos(angle)
-        y = 400 + distance * math.sin(angle)
-        screen.set_at((int(x), int(y)), (0,0,0))
-    pygame.display.flip()
+    screen = pygame.display.set_mode((800, 800))
+    particles = [(random.gauss(0, 0.5), random.uniform(0, 6.28318)) for i in range(2000)]
+
+    for i in range(radio):
+        screen.fill((255,255,255))
+        for speed, angle in particles:
+            distance = i * speed
+            x = pos_x + distance * math.cos(angle)
+            y = pos_y + distance * math.sin(angle)
+            screen.set_at((int(x), int(y)), (0,0,0))
+        pygame.display.flip()
+
+explotar(400, 400, 400)
