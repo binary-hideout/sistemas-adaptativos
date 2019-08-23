e3bc2d792869c29919178f12c3c9a506504104a3
netotz <neto.otz@hotmail.com>
2019-08-22 20:24:01 -0500

    Simplificar función
    Ahora solo hay un solo for en la función secuencia_explosiones

--- tarea1/fuegos.py
+++ tarea1/fuegos.py
@@ -3,8 +3,6 @@ import random
 import pygame
 from sys import argv
 
-pygame.init()
-
 def explotar(pos_x, pos_y, radio, rgb_tuple):
     print("   Posición X:", pos_x)
     print("   Posición Y:", pos_y)
@@ -12,7 +10,7 @@ def explotar(pos_x, pos_y, radio, rgb_tuple):
     print("   Color en RGB:", rgb_tuple, "\n")
 
     screen = pygame.display.set_mode((800, 800))
-    particles = [(random.gauss(0, 10), random.uniform(0, 6.28318)) for i in range(random.randint(500, 9000))]
+    particles = [(random.gauss(0, 0.5), random.uniform(0, 6.28318)) for i in range(random.randint(50, 5000))]
 
     for i in range(radio):
         screen.fill((0, 0, 0))
@@ -23,26 +21,35 @@ def explotar(pos_x, pos_y, radio, rgb_tuple):
             screen.set_at((int(x), int(y)), rgb_tuple)
         pygame.display.flip()
 
-def secuencia_explosiones(tipo, cantidad, rgb_list = list()):
-    """Genera 'n' explosiones en un patrón de diagonal.
+def secuencia_explosiones(cantidad, tipo = "diagonal", rgb_list = list()):
+    """Genera la 'cantidad' de explosiones en el patrón especificado por 'tipo'.
     """
-    screen_divide = math.floor(800 / cantidad)
     # si la lista de colores está vacía
     if len(rgb_list) == 0:
         rgb_list = generarListaRGB(cantidad)
 
-    if tipo.lower() == "horizontal":
-        for i in range(cantidad):
-            explotar(i * screen_divide, 500, 500, rgb_list[i])
-    elif tipo.lower() == "vertical":
-        for i in range(cantidad):
-            explotar(500, i * screen_divide, 500, rgb_list[i])
-    elif tipo.lower() == "diagonal":
-        for i in range(cantidad):
-            explotar(i * screen_divide, i * screen_divide, 500, rgb_list[i])
-    elif tipo.lower() == "random":
-        for i in range(cantidad):
-            explotar(random.randint(0, 800), random.randint(0, 800), 500, rgb_list[i])
+    step_x = 700 / cantidad
+    step_y = step_x
+    if tipo == "horizontal":
+        pos_x = step_x
+        pos_y = 400
+        step_y = 0
+    elif tipo == "vertical":
+        pos_x = 400
+        step_x = 0
+        pos_y = step_y
+    elif tipo == "diagonal":
+        step_x = math.sqrt(2 * 400 * 400) / cantidad
+        step_y = step_x
+        pos_x, pos_y = step_x, step_y
+
+    for i in range(cantidad):
+        print(i + 1, "° explosión", sep='')
+        radio = random.randint(200, 600)
+        explotar(pos_x, pos_y, radio, rgb_list[i])
+
+        pos_x += step_x
+        pos_y += step_y
 
 def generarListaRGB(size):
     '''Genera una lista de tuples. Cada tuple contiene un color aleatorio en formato RGB.
@@ -59,6 +66,8 @@ def generarListaRGB(size):
 
     return rgb_list
 
+pygame.init()
+
 print("3 llamadas a la función 'explotar'")
 explotar(255, 500, 333, (0, 0, 255))
 explotar(500, 100, 880, (0, 255, 0))
@@ -66,10 +75,15 @@ explotar(300, 221, 670, (255, 0, 0))
 
 # validar si el comando contiene el argumento para la cantidad de explosiones
 try:
-    arg_tipo = argv[1]
-    arg_ciclos = int(argv[2])
+    arg_ciclos = int(argv[1])
 except:
     arg_ciclos = 2
 
+# validar comando de tipo de patrón
+try:
+    arg_tipo = argv[2]
+except:
+    arg_tipo = "diagonal"
+
 print("Llamada a la función 'secuencia_explosiones'")
-secuencia_explosiones(arg_tipo, arg_ciclos)
+secuencia_explosiones(arg_ciclos, arg_tipo.lower())
