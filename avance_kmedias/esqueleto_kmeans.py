3c7dd2277074f176480ef5f97387e45c63d1f5dd
netotz <neto.otz@hotmail.com>
2019-09-09 10:41:24 -0500

    Agregar función que regresa centroide más cercano

--- avance_kmedias/esqueleto_kmeans.py
+++ avance_kmedias/esqueleto_kmeans.py
@@ -18,12 +18,12 @@ Eleva numero a la potencia indicada.
 '''
 
 from math import sqrt
-import sys
+from sys import float_info
 
 def calcularDistanciaEuclideana(puntoA, puntoB):
     ''' (A) Primer funcion a completar 
     Entradas: puntoA y puntoB -- son listas numericas de cualquier longitud (debe ser la misma longitud en ambas listas).
-    Salida: Distancia euclidiana entre las listas.''' 
+    Salida: Distancia euclidiana entre las listas.'''
     return sqrt((puntoB[0] - puntoA[0]) ** 2 + (puntoB[1] - puntoA[1]) ** 2 + (puntoB[2] - puntoA[2]) ** 2)
 
 def actualizarCentroide(datos, grupos, indiceCentroide):
@@ -35,6 +35,18 @@ def actualizarCentroide(datos, grupos, indiceCentroide):
         Salida: lista que contiene los nuevos valores para el centroide cuyo indice es indiceCentroide
     '''
     pass
+
+def centroideMasCercano(centroides, muestra):
+    '''Recibe una 'muestra' que almacena una colección de los datos y 'centroides' que almacena una colección de los centroides.
+    Regresa 'k' que es la posición del centroide más cercano.
+    '''
+    menor = float_info.max()
+    for i in range(3):
+        dist = calcularDistanciaEuclideana(muestra, centroides[i])
+        if dist < menor:
+            menor = dist
+            k = i
+    return k
 
 datos = ((153, 51, 255), (121, 236, 221), (209, 236, 121), (240, 164, 76), (240, 98, 76), (76, 93, 240), (50, 239, 94))
 centroides = ((255, 0, 0), (0, 255, 0), (0, 0, 255)) #Inicializacion de centroides
