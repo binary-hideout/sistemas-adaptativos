743964eaa487da2350f5bc555056560bd6dfe6b3
netotz <neto.otz@hotmail.com>
2019-09-14 13:02:47 -0500

    Agregar comentarios

--- perceptron/esqueleto_perceptron.py
+++ perceptron/esqueleto_perceptron.py
@@ -16,7 +16,9 @@ def funcion_activacion(resultado_nucleo):
 	'''
 	return 1 if resultado_nucleo >= 0 else 0
 
+# aleatorizar pesos iniciales
 pesos = [random() for _ in range(7)]
+# matriz de entradas
 entradas = [[-1, 1, 0, 1, 0, 0, 0],
 			[-1, 1, 0, 1, 1, 0, 0],
 			[-1, 1, 0, 1, 0, 1, 0], 
@@ -31,28 +33,33 @@ entradas = [[-1, 1, 0, 1, 0, 0, 0],
 			[-1, 0, 0, 0, 1, 0, 1],
 			[-1, 0, 1, 1, 0, 1, 1],
 			[-1, 0, 1, 1, 1, 0, 0]]
+# salidas esperadas
 salidas = [1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0]
 
-max_iter = 1000
+max_iter = 100
 tasa_aprendizaje = 0.1
 iteracion = 0
-pincorrectos = 1.0
+pincorrectos = 1
 while pincorrectos > 0 and iteracion < max_iter:
 	print("\nITERACION %d" % iteracion)
 	incorrectos = 0
 
+	# entrenamiento
 	for entrada, salida in zip(entradas, salidas):
 		print("\nProcesando entrada:", entrada)
 		print("Salida esperada:", salida)
 
+		# predecir la salida de la entrada actual
 		prediccion = funcion_activacion(funcion_nucleo(pesos, entrada))
 		print("Predicción:", prediccion)
+		# comparar la predicción con la salida esperada
 		if prediccion != salida:
 			incorrectos += 1
 
 			print("\n-----ACTUALIZANDO PESOS-----")
 			error = salida - prediccion
 			for i, x_i in zip(range(len(pesos)), entrada):
+				# nuevo peso
 				pesos[i] += tasa_aprendizaje * x_i * error
 				print("w%d = %g" % (i, pesos[i]))
 	pincorrectos = incorrectos * 1.0 / len(entradas)
