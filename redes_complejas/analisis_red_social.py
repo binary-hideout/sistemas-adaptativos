16ca2fccba421f206ded9d416b0ace6a7ef80c31
netotz <neto.otz@hotmail.com>
2019-10-10 16:28:56 -0500

    Agregar práctica de redes complejas

---
+++ redes_complejas/analisis_red_social.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+def calcular(lines):
+	n = len(lineas)
+	m = 0
+	grados = list()
+
+	for linea in lineas:
+		linea = linea.strip()
+		celdas = linea.split()
+		grado = 0
+		for celda in celdas:
+			m += int(celda)
+			grado += int(celda)
+		grados.append(grado)
+	m /= 2
+
+	densidad = (2 * m) / (n * (n - 1))
+
+	print('n:', n)
+	print('m:', m)
+	print('Densidad:', densidad)
+
+	for i in range(len(grados)):
+		centralidad = grados[i] / (n - 1)
+		print("Grado v" + str(i) + " = " + str(grados[i]) + " Centralidad: " + str(centralidad))
+
+try:
+	archivo = open(argv[1],'r')
+	lineas = archivo.readlines()
+	archivo.close()
+	calcular(lineas)
+except IOError:
+	print('No se pudo abrir el archivo')
