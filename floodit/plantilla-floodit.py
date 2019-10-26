208dc46da98220be19dfe48bd418ccc429567424
netotz <neto.otz@hotmail.com>
2019-10-25 19:01:16 -0500

    Agregar archivos de Flood It

---
+++ floodit/plantilla-floodit.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+import sys
+
+root=tk.Tk()
+c=tk.Canvas(root,width=280,height=280)
+
+#CODIFICA AQUI
+
+#Codigo ejemplo para crear un rectangulo
+#c.create_rectangle(0,0,20,20,fill="#ff0000",outline="")
+
+c.pack()
+c.mainloop()
+
+
+	
